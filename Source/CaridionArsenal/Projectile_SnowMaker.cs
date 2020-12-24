3b8fb3add8af7d6bd8f76f881e0eae314505accf
emipa606 <emil.offermo@litet.net>
2020-12-24 22:29:17 +0100

    First publish

---
+++ Source/CaridionArsenal/Projectile_SnowMaker.cs
@@ -0,0 +1,33 @@
+﻿using Verse;
+
+namespace CaridionArsenal
+{
+    // Token: 0x02000002 RID: 2
+    internal class Projectile_SnowMaker : Projectile
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+		protected override void Impact(Thing hitThing)
+		{
+			GenExplosion.DoExplosion(Position, Map, def.projectile.explosionRadius, def.projectile.damageDef, launcher, def.projectile.GetDamageAmount(launcher) , -1, SoundDef.Named("Interact_Ignite"), def, equipmentDef, null, null, 0f, 1, false, null, 0f, 1);
+			SnowCreation(Position, def.projectile.explosionRadius, Map);
+			CellRect.CenteredOn(Position, 3).ClipInsideMap(Map);
+			base.Impact(hitThing);
+		}
+
+		// Token: 0x06000002 RID: 2 RVA: 0x00002104 File Offset: 0x00000304
+		protected void SnowCreation(IntVec3 pos, float radius, Map map)
+		{
+			var depthToAdd = 10f;
+			foreach (IntVec3 a in GenRadial.RadialPatternInRadius(radius))
+			{
+				IntVec3 c = a + pos;
+				var flag = c.InBounds(map);
+				if (flag)
+				{
+					map.snowGrid.AddDepth(c, depthToAdd);
+				}
+			}
+			map.snowGrid.AddDepth(pos, depthToAdd);
+		}
+	}
+}
---
+++ Source/CaridionArsenal/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("CaridionArsenal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CaridionArsenal")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("206b710e-2ed2-4eb1-a586-eb5c4a24279e")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
